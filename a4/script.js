9f273ce35240b1af388d4b657980094e5006d129
Omri Cundangan <37829365+dlrht@users.noreply.github.com>
2020-10-07 14:54:14 -0400

    A4 complete & reorganized
    - A4 script now a lot easier to understand & more organized
    - better file organization

--- a4/script.js
+++ a4/script.js
@@ -1,181 +1,76 @@
 
-
-let table_data = [
+table_data2 = [
     {
-        'filename': 'Time',
-        'algo': 'MP3',
-        'bitrate': '96kbps',
-        'oSize': '10501',
-        'cSize': '716',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_96.mp3',
-        'rAudio': 'aud/time_mp3_96.wav',
+        'title' : 'Time',
+        'algo' : 'OGG',
+        'quality' : [0, 3, 6, 10],
+        'quality_suffix' : ' / 10',
+        'oSize' : 10501,
+        'cSize' : [ 414, 772, 1287, 3153 ],
+        'filename_prefix' : 'time'
     },
     {
-        'filename': 'Time',
-        'algo': 'MP3',
-        'bitrate': '144kbps',
-        'oSize': '10501',
-        'cSize': '1074',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_144',
-        'rAudio': 'aud/time_mp3_144.wav',
+        'title' : 'Time',
+        'algo' : 'MP3',
+        'quality' : [96, 144, 192, 320],
+        'quality_suffix' : ' kbps',
+        'oSize' : 10501,
+        'cSize' : [ 716, 1074, 1431, 2385 ],
+        'filename_prefix' : 'time'
     },
     {
-        'filename': 'Time',
-        'algo': 'MP3',
-        'bitrate': '192kbps',
-        'oSize': '10501',
-        'cSize': '1431',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_192.mp3',
-        'rAudio': 'aud/time_mp3_192.wav',
+        'title' : 'Time',
+        'algo' : 'M4A',
+        'quality' : [10, 150, 350, 500],
+        'quality_suffix' : ' / 500',
+        'oSize' : 10501,
+        'cSize' : [ 1469, 1469, 1469, 1469 ],
+        'filename_prefix' : 'time'
     },
     {
-        'filename': 'Time',
-        'algo': 'MP3',
-        'bitrate': '320kbps',
-        'oSize': '10501',
-        'cSize': '2385',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_320.mp3',
-        'rAudio': 'aud/time_mp3_320.wav',
+        'title' : 'Time',
+        'algo' : 'AC3',
+        'quality' : [32, 96, 256, 640],
+        'quality_suffix' : ' / 640',
+        'oSize' : 10501,
+        'cSize' : [ 239, 715, 1905, 4762 ],
+        'filename_prefix' : 'time'
     },
     {
-        'filename': 'Time',
-        'algo': 'AC3',
-        'bitrate': '32/640',
-        'oSize': '10501',
-        'cSize': '239',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_32.ac3',
-        'rAudio': 'aud/time_ac3_32.wav',
+        'title' : 'Hot Pursuit',
+        'algo' : 'OGG',
+        'quality' : [0, 3, 6, 10],
+        'quality_suffix' : ' / 10',
+        'oSize' : 10501,
+        'cSize' : [ 470, 838, 1448, 3608],
+        'filename_prefix' : 'hp'
     },
     {
-        'filename': 'Time',
-        'algo': 'AC3',
-        'bitrate': '96/640',
-        'oSize': '10501',
-        'cSize': '715',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_96.ac3',
-        'rAudio': 'aud/time_ac3_96.wav',
+        'title' : 'Hot Pursuit',
+        'algo' : 'MP3',
+        'quality' : [96, 144, 192, 320],
+        'quality_suffix' : ' kbps',
+        'oSize' : 10501,
+        'cSize' : [ 705, 1056, 1408, 2346 ],
+        'filename_prefix' : 'hp'
     },
     {
-        'filename': 'Time',
-        'algo': 'AC3',
-        'bitrate': '256/640',
-        'oSize': '10501',
-        'cSize': '1905',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_256.ac3',
-        'rAudio': 'aud/time_ac3_256.wav',
+        'title' : 'Hot Pursuit',
+        'algo' : 'M4A',
+        'quality' : [10, 150, 350, 500],
+        'quality_suffix' : ' / 500',
+        'oSize' : 10501,
+        'cSize' : [ 1446, 1446, 1446, 1446 ],
+        'filename_prefix' : 'hp'
     },
     {
-        'filename': 'Time',
-        'algo': 'AC3',
-        'bitrate': '640/640',
-        'oSize': '10501',
-        'cSize': '4762',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_640.ac3',
-        'rAudio': 'aud/time_ac3_640.wav',
-    },
-    {
-        'filename': 'Time',
-        'algo': 'M4A',
-        'bitrate': '10/500',
-        'oSize': '10501',
-        'cSize': '1469',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_10.m4a',
-        'rAudio': 'aud/time_m4a_10.wav',
-    },
-    {
-        'filename': 'Time',
-        'algo': 'M4A',
-        'bitrate': '150/500',
-        'oSize': '10501',
-        'cSize': '1469',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_150.m4a',
-        'rAudio': 'aud/time_m4a_150.wav',
-    },
-    {
-        'filename': 'Time',
-        'algo': 'M4A',
-        'bitrate': '350/500',
-        'oSize': '10501',
-        'cSize': '1469',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_350.m4a',
-        'rAudio': 'aud/time_m4a_350.wav',
-    },
-    {
-        'filename': 'Time',
-        'algo': 'M4A',
-        'bitrate': '500/500',
-        'oSize': '10501',
-        'cSize': '1469',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_500.m4a',
-        'rAudio': 'aud/time_m4a_500.wav',
-    },
-    {
-        'filename': 'Time',
-        'algo': 'OGG',
-        'bitrate': '0/10',
-        'oSize': '10501',
-        'cSize': '414',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_0.ogg',
-        'rAudio': 'aud/time_ogg_0.wav',
-    },
-    {
-        'filename': 'Time',
-        'algo': 'OGG',
-        'bitrate': '3/10',
-        'oSize': '10501',
-        'cSize': '772',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_3.ogg',
-        'rAudio': 'aud/time_ogg_3.wav',
-    },
-    {
-        'filename': 'Time',
-        'algo': 'OGG',
-        'bitrate': '6/10',
-        'oSize': '10501',
-        'cSize': '1287',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_6.ogg',
-        'rAudio': 'aud/time_ogg_6.wav',
-    },
-    {
-        'filename': 'Time',
-        'algo': 'OGG',
-        'bitrate': '10/10',
-        'oSize': '10501',
-        'cSize': '3153',
-        'cRatio': '5',
-        'oAudio': 'aud/time_orig.wav',
-        'cAudio': 'aud/time_10.ogg',
-        'rAudio': 'aud/time_ogg_10.wav',
+        'title' : 'Hot Pursuit',
+        'algo' : 'AC3',
+        'quality' : [32, 96, 256, 640],
+        'quality_suffix' : ' / 640',
+        'oSize' : 10501,
+        'cSize' : [ 235, 704, 1876, 4689 ],
+        'filename_prefix' : 'hp'
     },
 ]
 
@@ -197,26 +92,61 @@ function discography(){
     table.appendChild(thead);
 
     tbody = document.createElement('tbody');
-    for (row = 0; row < table_data.length; row++) {
-        tr = document.createElement('tr');
-        let columns = Object.keys(table_data[row]);
-        for (col = 0; col < Object.keys(table_data[row]).length; col++) {
-          td = document.createElement('td');
-          tr.appendChild(td);
-            if (col == 5){
-                oSize = parseFloat(table_data[row][columns[col-2]]);
-                cSize = parseFloat(table_data[row][columns[col-1]]);
-                cRatio = (cSize/oSize).toFixed(4);
-                td.innerHTML = cRatio;
-            }
-            else if (col >= 6){
-                td.innerHTML = "<a href=" + table_data[row][columns[col]] +">Link</a>"
-            }
-            else{
-                td.innerHTML = table_data[row][columns[col]];
-            }
+    for (i = 0; i < table_data2.length; i++) {
+        for (j = 0; j < table_data2[i]['quality'].length; j++){
+            tr = document.createElement('tr');
+
+            // Title
+            td = document.createElement('td');
+            td.innerHTML = table_data2[i]['title'];
+            tr.appendChild(td);
+
+            // Algorithm
+            td = document.createElement('td');
+            td.innerHTML = table_data2[i]['algo'];
+            tr.appendChild(td);
+
+            // Bit-rate/quality
+            td = document.createElement('td');
+            td.innerHTML = table_data2[i]['quality'][j] + table_data2[i]['quality_suffix'];
+            tr.appendChild(td);
+
+            // Original size
+            td = document.createElement('td');
+            td.innerHTML = table_data2[i]['oSize'];
+            tr.appendChild(td);
+
+            // Compressed size
+            td = document.createElement('td');
+            td.innerHTML = table_data2[i]['cSize'][j];
+            tr.appendChild(td);
+
+            // Compression ratio
+            td = document.createElement('td');
+            td.innerHTML = (table_data2[i]['cSize'][j] / table_data2[i]['oSize']).toFixed(4);
+            tr.appendChild(td);
+
+            // Link to original audio
+            fName = table_data2[i]['filename_prefix'];
+            algorithm = table_data2[i]['algo'].toLowerCase();
+            quality = table_data2[i]['quality'][j];
+            
+            td = document.createElement('td');
+            td.innerHTML = `<a href=aud/${fName}/${fName}_orig.wav>Link</a>`;
+            tr.appendChild(td);
+
+            // Link to compressed audio
+            td = document.createElement('td');
+            td.innerHTML = `<a href=aud/${fName}/${fName}_${algorithm}_${quality}.${algorithm}>Link</a>`;
+            tr.appendChild(td);
+
+            // Link to residual audio
+            td = document.createElement('td');
+            td.innerHTML = `<a href=aud/${fName}/${fName}_${algorithm}_${quality}_r.wav>Link</a>`;
+            tr.appendChild(td);
+
+            tbody.appendChild(tr);
         }
-        tbody.appendChild(tr);
       }
       table.appendChild(tbody);
       document.getElementById("discography_table").appendChild(table);
